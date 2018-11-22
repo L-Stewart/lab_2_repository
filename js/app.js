b5ee1506d7acacd1692b22053bc15af02fd47dc6
jjblues86 <jeromejoof@yahoo.com>
2018-11-21 19:19:46 -0800

    Feature 1 Complete

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const allHorn = [];
+
+function Horn (obj){
+  this.title = obj.title;
+  this.image_url = obj.image_url;
+  this.description = obj.description;
+
+  allHorn.push(this);
+}
+
+Horn.prototype.render = function () {
+  $('main').append('<section class="picture"></section>');
+  let $picture = $('section[class="picture"]');
+  let picTemplate = $('#photo-template').html();
+
+  $picture.html(picTemplate);
+  $picture.find('h2').text(this.title);
+  $picture.find('img').attr('src',this.image_url);
+  $picture.find('p').text(this.description);
+  
+  $picture.removeClass('picture');
+  $picture.attr('class', this.title);
+
+}
+
+function readJson() {
+  $.get('./data/page-1.json', 'json')
+  .then(data =>{
+    data.forEach(hornObj =>{
+      new Horn(hornObj);
+    })
+  })
+  .then(() => {allHorn.forEach(horn => {
+    horn.render();
+  })
+})
+}
+
+$(() => readJson());
